c71676f131dac79bcc4ea980891b42f7ff8359fd
eddyermonta <eduardo.montano@utp.edu.co>
2021-07-05 21:46:32 -0500

    comentado

--- juego.py
+++ juego.py
@@ -1,25 +1,21 @@
 import pygame
 import time
 import numpy as np
-windows=pygame
 
+#inicializamos pygame
+windows=pygame
 pygame.init()
 pantalla=pygame.display.set_mode((800,475))
 
-Blanco=(255,255,255)
-Rojo=(0,0,255)
-Amarillo=(255,255,0)
-Azul=(0,255,0)
-
 TrikiTriki=np.zeros((3,3))
-
+#carga el inicio de la imagen
 def inicio():
     presentar=pygame.image.load("D:/Usuario/Documentos/Eduardo/Utp/semestre 5/computacion grafica/parte3/Triki-Triki/Imagenes/Inicio.png")
     pantalla.blit(presentar,(0,0))
     pygame.display.update()
     time.sleep(1)
     menu()
-
+#cargamos el menu de opciones
 def menu():
     menu=pygame.image.load("D:/Usuario/Documentos/Eduardo/Utp/semestre 5/computacion grafica/parte3/Triki-Triki/Imagenes/Menu.png")
     pantalla.blit(menu,(0,0))
@@ -46,7 +42,7 @@ def menu():
                     
         pantalla.blit(menu,(0,0))
         pygame.display.update()
-
+#cargamos la ventana de jugar
 def jugar(turno):
     jugar=pygame.image.load("D:/Usuario/Documentos/Eduardo/Utp/semestre 5/computacion grafica/parte3/Triki-Triki/Imagenes/Game.png")
     pantalla.blit(jugar,(0,0))
@@ -60,6 +56,8 @@ def jugar(turno):
                 x,y= eventos.pos
                 print(x," ",y," ")    
                 if (x >= 255 and y >=93 and x<=337 and y<=172):
+                   #en marcar le estamos pasando donde empieza a dibujar
+                   #el turno dado y la posicion de la matriz
                    turno= marcar([255,90],turno,[0,0])
                 if (x>=361 and y>=90 and x<=443 and y<=169):
                     turno= marcar([361,90],turno,[0,1])
@@ -78,7 +76,8 @@ def jugar(turno):
                 if (x>=470 and y>=284 and x<=550 and y<=359):
                     turno= marcar([470,284],turno,[2,2]) 
                 validarJuego()
-
+#asignamos segun el caso x o circulo a la pantalla y validamos
+#validamos que casilla ya tienen x o circulo si es asi no puede marcar
 def marcar(ini,turno,pos):
     if(TrikiTriki[pos[0]][pos[1]]==0):
         if(turno == "x"):
@@ -98,26 +97,32 @@ def marcar(ini,turno,pos):
          print("ya tiene una marca dibujada")
     return turno      
 
+#regresar al menu y reiniciar la matriz de juego
 def RetornoMenu():
     print("gano")
     TrikiTriki[:,:]=0
     time.sleep(1)
     menu()
-
+#vakuda cuando ganamos o cuando empatamos
 def validarJuego():
     for i in range(3):
+        #valida que las casiilas de izquierda a derecha sean iguales pa saber si gano lateralmente
         if((TrikiTriki[i][0]==TrikiTriki[i][1] and TrikiTriki[i][2]==TrikiTriki[i][1] and TrikiTriki[i][0]!=0)
+        #valida las casillas de arriba a abajo
         or (TrikiTriki[0][i]==TrikiTriki[1][i] and TrikiTriki[2][i]==TrikiTriki[1][i] and TrikiTriki[0][i]!=0)
+        #valida diagonal izquierda 
         or (i+1<3 and TrikiTriki[i][i]==TrikiTriki[i+1][i+1] and TrikiTriki[i][i]== TrikiTriki[2][2] and 
             TrikiTriki[i][i]!=0)
+        #valida diagonal derecha
         or(i==2 and TrikiTriki[i][0]==TrikiTriki[0][i] and TrikiTriki[1][1]==TrikiTriki[i][0] 
             and TrikiTriki[i][0]!=0)):
             print("gano")
             RetornoMenu()
+        #empata si todas las casillas estan marcadas
         elif(np.all(TrikiTriki[:,:])>0):
             print("empato")
             RetornoMenu()
-
+#menu de ayuda
 def helpMe():
     advice=pygame.image.load("D:/Usuario/Documentos/Eduardo/Utp/semestre 5/computacion grafica/parte3/Triki-Triki/Imagenes/Advice.png")
     pantalla.blit(advice,(0,0))
@@ -125,8 +130,8 @@ def helpMe():
     while(True):
         for eventos in pygame.event.get():
             if eventos.type == pygame.QUIT:
-                TrikiTriki[:,:]=0
                 menu()
+#menu de nosotros
 def About():
     about=pygame.image.load("D:/Usuario/Documentos/Eduardo/Utp/semestre 5/computacion grafica/parte3/Triki-Triki/Imagenes/About.png")
     pantalla.blit(about,(0,0))
@@ -134,7 +139,6 @@ def About():
     while(True):
         for eventos in pygame.event.get():
             if eventos.type == pygame.QUIT:
-                TrikiTriki[:,:]=0
                 menu()
 
 inicio()   
